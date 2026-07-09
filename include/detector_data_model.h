75f217870e24325f18e5d58b05871b855f1a8e7b
Mob1le <bessalovs@gmail.com>
2026-07-09 08:07:03 +0300

    Рtализован DetectorDataModel

--- include/detector_data_model.h
+++ include/detector_data_model.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include "detector.h"
 #include <QAbstractTableModel>
-#include <QVector>
+#include <QHash>
+#include <QMutex>
 
 class DetectorDataModel : public QAbstractTableModel
 {
     Q_OBJECT
+private:
+    QHash<qint64, Detector> dataHash;
+    mutable QMutex mutex;
 public:
-    DetectorDataModel(QObject *parent = nullptr);
+    explicit DetectorDataModel(QObject *parent = nullptr);
     ~DetectorDataModel() override;
 
     
@@ -16,4 +21,6 @@ public:
     int columnCount (const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    void updateData(const Detector &detector);
 };
--- src/detector_data_model.cpp
+++ src/detector_data_model.cpp
@@ -1,4 +1,4 @@
-#include "detector_model.h"
+#include "detector_data_model.h"
 
 DetectorDataModel::DetectorDataModel(QObject *parent)
 {
@@ -10,20 +10,61 @@ DetectorDataModel::~DetectorDataModel()
 
 int DetectorDataModel::rowCount(const QModelIndex &parent) const
 {
-    return 0;
+    QMutexLocker locker(&mutex);
+    return dataHash.size();
 }
 
+// Поле: ID (qint64), значение (float), время (QDateTime). = 3
 int DetectorDataModel::columnCount(const QModelIndex &parent) const
 {
-    return 0;
+    return 3;
 }
 
 QVariant DetectorDataModel::data(const QModelIndex &index, int role) const
 {
-    return QVariant();
+    if (!index.isValid() || index.row() < 0 || index.row() >= dataHash.size())
+    {
+        return QVariant();
+    }
+
+    QMutexLocker locker(&mutex);
+
+    const Detector &det = dataHash.values()[index.row()];
+
+    switch (index.column()) {
+    case 0:  // ID
+        return role == Qt::DisplayRole ? QString::number(det.getId()) : QVariant();
+    case 1:  // Value
+        return role == Qt::DisplayRole ? QString::number(det.getValue(), 'f', 2) : QVariant();
+    case 2:  // Time
+        return role == Qt::DisplayRole ? det.getTimestamp().toString("dd.MM.yyyy HH:mm:ss") : QVariant();
+    default:
+        return QVariant();
+    }
+
 }
 
 QVariant DetectorDataModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    return QVariant();
+    if (role != Qt::DisplayRole) return QVariant();
+    if (orientation != Qt::Horizontal) return QVariant();
+
+    switch (section) {
+    case 0: return "ID";
+    case 1: return "Значение";
+    case 2: return "Время";
+    default: return QVariant();
+    }
+
+}
+
+void DetectorDataModel::updateData(const Detector &detector)
+{
+    QMutexLocker locker(&mutex);
+
+    if (!dataHash.contains(detector.getId()))
+    {
+        return;
+    }
+    //TODO
 }
